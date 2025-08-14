2343f4318466a695bd8118c58f1859f6e5912a6e
BrunoNolasco <brunonolasco2763@gmail.com>
2025-08-14 11:06:31 +0100

    log pronto

--- log_in.py
+++ log_in.py
@@ -1,29 +1,42 @@
+import mysql.connector as mysql
+import datetime as date
 
-import datetime  as date
+SpecialSym = ['$', '@', '#', '%']
 
-SpecialSym =['$', '@', '#', '%']
-dates1=date.datetime.now()
-
+conexao = mysql.connect(
+    host="localhost",
+    user="Brunopy",
+    password="",
+    database='controle_financeiro'
+)
+cursor = conexao.cursor()
 
 def logii():
+    
+    conexao = mysql.connect(
+        host="localhost",
+        user="Brunopy",
+        password="",
+        database="controle_financeiro"
+    )
+    cursor = conexao.cursor()
+    
     while True:
         print("\n")
         print("___________________________________________________________________________")
         print("                      Bem-vindo à Personal Finance                      ")
-        print("____________________________________________________________________________")
+        print("___________________________________________________________________________")
         print("Aperte 1 para Sign Up ")
         print("Aperte 2 para Sign In ")
 
         try:
-            ch=int(input("Introduza sua escolha: "))
-            if str(ch).isalpha() or ch >= 3:
+            ch = int(input("Introduza sua escolha: "))
+            if ch >= 3:
+                raise ValueError("Opção inválida")
 
-                raise Exception ("Não é um número válido, tente novamente: ")
-
-
-
-
-            elif(ch==1):
+            # --- SIGN UP ---
+            if ch == 1:
+                # --- Nome ---
                 while True:
                     try: 
                         a='.'
@@ -37,11 +50,13 @@ def logii():
                         a8='?'
                         a9='/'
                         ut=input("Introduza o seu nome: ")
-                        if (a in ut) or (a1 in ut) or (a3 in ut) or (a4 in ut) or (a5 in ut) or (a6 in ut) or (a7 in ut) or (a8 in ut) or (a9 in ut):
+                        if (a in ut) or (a2 in ut) or (a1 in ut) or (a3 in ut) or (a4 in ut) or (a5 in ut) or (a6 in ut) or (a7 in ut) or (a8 in ut) or (a9 in ut):
                             raise TypeError 
                         break
                     except TypeError:
-                        print("Caracteres especiais como . , @ # < > ? / ; não são aceitos. ")
+                        print("Caracteres especiais como . , @ # < > ? / ; não são aceitos.")
+
+                # --- Sobrenome ---
                 while True:
                     try:
                         a='.'
@@ -51,79 +66,101 @@ def logii():
                         a4='&'
                         a5='='
                         a6=','
+                        a7='@'
                         a8='?'
-                        a9='@'
+                        a9='/'
                         p=input("Introduza o seu sobrenome: ")
-                        if(a in p) or (a1 in p) or (a2 in p) or (a3 in p) or (a4 in p) or (a5 in p) or (a6 in p) or (a7 in p) or (a8 in p) or (a9 in p):
+                        if (a in p) or (a2 in p) or (a1 in p) or (a3 in p) or (a4 in p) or (a5 in p) or (a6 in p) or (a7 in p) or (a8 in p) or (a9 in p):
                             raise TypeError 
                         break
-                    except TypeError as m:
-                        print("Caracteres especiais como . , @ # < > ? / ; não são aceitos. ")
-                while True:        
-                    try:
+                    except TypeError:
+                        print("Caracteres especiais como . , @ # < > ? / ; não são aceitos.")
 
-                        z=input("Introduza sua Password: ")
-                        if (len(z)<6):
-                            raise ValueError ("A password deve ter pelo menos 6 caracteres. ")
+                # --- Senha ---
+                while True:
+                    try:
+                        z = input("Introduza a sua Password: ")
+                        if len(z) < 6:
+                            raise ValueError
                         if not any(char.isdigit() for char in z):
-                            raise ValueError ("A password deve ter pelo menos um número. ")
+                            raise ValueError
                         if not any(char.isupper() for char in z):
-                            raise KeyError ()
+                            raise ValueError
                         if not any(char.islower() for char in z):
-                            raise KeyError
-                        if not any(char in SpecialSym for char in z ):
-                            raise KeyError
+                            raise ValueError
+                        if not any(char in SpecialSym for char in z):
+                            raise ValueError
                         break
                     except ValueError:
-                        print("A password deve conter pelo menos 1 letra maiúscula, 1 letra minúscula, um caracter especial e pelo menos 7 caracteres")
-                    except KeyError:
-                        print("A password deve conter pelo menos 1 letra maiúscula, 1 letra minúscula, um caracter especial e pelo menos 7 caracteres")
+                        print("A senha deve conter 1 letra maiúscula, 1 letra minúscula, 1 número, 1 caracter especial e pelo menos 6 caracteres.")
 
+                # --- Depósito de abertura ---
+                k = str(input("Introduza quanto dinheiro pretende depositar: "))
 
-                k=int(input("Introduza quanto dinheiro pretende depositar: "))
+                # --- Data de nascimento ---
                 while True:
                     try:
-                        a=input("Introduza sua data de nascimento: ")
-                        a = date.datetime.strptime(a, "%d/%m/%Y").date()
+                        a = input("Introduza sua data de nascimento (DD/MM/AAAA): ")
+                        nascimento = date.datetime.strptime(a, "%d/%m/%Y").date()
                         break
                     except ValueError:
-                        print("Por favor, introduza a data de nascimento no formato DD/MM/YYYY")
+                        print("Formato inválido. Use DD/MM/AAAA.")
 
-                s=input("Introduza seu endereço: ")
+                # --- Endereço ---
+                s = input("Introduza seu endereço: ")
+
+                # --- Telefone ---
                 while True:
                     try:
-                        d=input("Introduza seu número de telefone: ")
-                        if (len(d)!=9) or (d.isalpha()):
-                            raise ValueError("Por favor, introdura um número válido de 9 dígitos")
+                        d = input("Introduza seu número de telefone: ")
+                        if len(d) != 9 or not d.isdigit():
+                            raise ValueError("Número de telefone inválido")
                         break
                     except ValueError as m:
                         print(m)
+
+                # --- Contribuinte ---
                 while True:
                     try:
-                        f=input("Introduza seu contribuinte: ")
-                        if(len(f)!=9) or (f.isalpha()):
-                            raise ValueError ("Por favor, introdura um número válido de 9 dígitos")
+                        f = input("Introduza seu contribuinte: ")
+                        if len(f) != 9 or not f.isdigit():
+                            raise ValueError("Contribuinte inválido")
                         break
                     except ValueError as m:
                         print(m)
-                #q="insert into bank values('{}','{}','{}','{}','{}','{}','{}','{}')".format(ut,p,z,a,s,d,f,k)
-                #mycur.execute(q)
-                #mycon.commit()
+
+                # --- Inserir no banco ---
+                query = """
+                INSERT INTO banco (nome, sobrenome, senha_hash, deposito_abertura, nascimento, endereço, telefone, contribuinte)
+                VALUES (%s, %s, SHA2(%s, 256), %s, %s, %s, %s, %s)
+                """
+                valores = (ut, p, z, k, nascimento, s, d, f)
+                cursor.execute(query, valores)
+                conexao.commit()
+
                 print("\n")
                 print("_________________________________________________________________________")
                 print("          Sua conta na Personal Finance foi criada com sucesso!          ")
                 print("_________________________________________________________________________")
-            break
-        except Exception:
-            print("\n")
-            print("_________________________________________________")
-            print(" Esse nome de usuário não é válido ")
-            print("_________________________________________________")
 
-        #while True:
-        #    if(ch==2):
-        #        u=input("Enter Your Username : ")
-        #        p=input("Enter Your Password : ")
-        #        a="select * from bank where UserName='{}' and Password='{}'".format(u,p)
-        #        mycur.execute(a)
-        #        data=mycur.fetchall()
+            # --- SIGN IN (básico) ---
+            elif ch == 2:
+                usuario = input("Digite seu nome: ")
+                senha = input("Digite sua senha: ")
+
+                query = "SELECT * FROM banco WHERE nome = %s AND senha_hash = SHA2(%s, 256)"
+                cursor.execute(query, (usuario, senha))
+                resultado = cursor.fetchone()
+
+                if resultado:
+                    print("Login efetuado com sucesso!")
+                else:
+                    print("Nome ou senha inválidos.")
+
+            break
+
+        except ValueError as e:
+            print(e)
+
+cursor.close()
+conexao.close()
--- test_log.py
+++ test_log.py
@@ -1,4 +1,6 @@
+import mysql.connector as mysql
+mysql
 from log_in import logii
 
 resultado_log = logii()
-print("O resultado da subtração é:", resultado_log)
+print("O resultado é:", resultado_log)
